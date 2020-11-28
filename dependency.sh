f3488f087f5e20335e37525e3590a71ef8b4bf6d
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2020-11-28 19:38:46 +0800

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -3,5 +3,4 @@
 sudo apt install apt-utils
 
 #needed cmd
-ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
-LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs/:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH
