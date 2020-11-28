c7677f8be65f7bd2b22b169456082f1f69a86428
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2020-11-28 19:49:47 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -3,7 +3,7 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -20,7 +20,7 @@ sudo add-apt-repository \
    stable"
    
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo docker run hello-world
 
