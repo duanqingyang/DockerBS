d1d52914324f81a71f83ac836e9aa287d13a83d0
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2020-11-29 12:35:37 +0800

    Update dist.sh

--- run_tools/dist.sh
+++ run_tools/dist.sh
@@ -12,7 +12,9 @@ export USE_BYTESCHEDULER=1
 # export BYTESCHEDULER_TIMELINE=timeline.json
 # export BYTESCHEDULER_DEBUG=1
 
-export COMMAND="python train_imagenet.py --network resnet --num-layers 18 --benchmark 1 \
+RUNFILE="/users/duanqing/byteps/bytescheduler/examples/mxnet-image-classification/train_imagenet.py"
+
+export COMMAND="python ${RUNFILE} --network resnet --num-layers 18 --benchmark 1 \
     --kv-store dist_sync --batch-size 32 --disp-batches 10 --num-examples 1000 --num-epochs 1" 
 
 
