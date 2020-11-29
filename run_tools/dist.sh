62619deda1cc952127e27b8d0e52cb7f3800174a
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2020-11-29 12:32:37 +0800

    Update dist.sh

--- run_tools/dist.sh
+++ run_tools/dist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set -x
 if [ $# -lt 3 ]; then
-    echo "usage: $0 num_servers num_workers role root_url bin [args..]"
+    echo "usage: $0 num_servers num_workers role root_url "
     exit -1;
 fi
 
@@ -12,6 +12,10 @@ export USE_BYTESCHEDULER=1
 # export BYTESCHEDULER_TIMELINE=timeline.json
 # export BYTESCHEDULER_DEBUG=1
 
+export COMMAND="python train_imagenet.py --network resnet --num-layers 18 --benchmark 1 \
+    --kv-store dist_sync --batch-size 32 --disp-batches 10 --num-examples 1000 --num-epochs 1" 
+
+
 export DMLC_NUM_SERVER=$1
 shift
 export DMLC_NUM_WORKER=$1
@@ -19,14 +23,11 @@ shift
 export DMLC_ROLE=$1
 shift
 export DMLC_PS_ROOT_URI=$1
-shift
-bin=$1
-shift
-arg="$@"
+
 
 export DMLC_PS_ROOT_PORT=8000
 # export DMLC_PS_ROOT_URI='127.0.0.1'
 # export DMLC_ROLE='scheduler'
-${bin} ${arg} &
+$COMMAND &
 
 wait
